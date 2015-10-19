f11bb8eb0ca9de89bee4f3031405cb7714740c90
andynartey <andy.nartey@googlemail.com>
2015-10-19 21:20:44 +0100

    Adding redirect and double integer fix to script

--- js/countdown.js
+++ js/countdown.js
@@ -8,34 +8,54 @@
 
 //clock starts here
 
-setInterval(function(){
-	// var future = new Date("October 28 2015 21:15:00 GMT+0200");
-    var future = new Date("October 30 2015");
-    var now = new Date();
-    var difference = Math.floor((future.getTime() - now.getTime()) / 1000);
+$(function () {
 
-    var seconds = fixIntegers(difference % 60);
-    difference = Math.floor(difference / 60);
+    // var future = new Date("October 19 2015 11:50:00 GMT+0200");
+    var url = "http://www.google.com"; // ex: "/index.php" , "../" , "/"
+    var future = new Date("October 28 2015");
 
-    var minutes = fixIntegers(difference % 60);
-    difference = Math.floor(difference / 60);
+    var _countdown = function () {
 
-    var hours = fixIntegers(difference % 24);
-    difference = Math.floor(difference / 24);
+        var now = new Date();
+        var difference = Math.floor((future.getTime() - now.getTime()) / 1000);
 
-    var days = difference;
+        //Timer reaches 0 - Redirect
+        if (difference <= 0) {
+            window.location = url;
+            difference = 0;
+        }
 
-    $(".seconds").html(seconds + "<span>SECONDS<\/span>");
-    $(".minutes").html(minutes + "<span>MINUTES<\/span>");
-    $(".hours").html(hours + "<span>HOURS<\/span>");
-    $(".days").html(days + "<span>DAYS<\/span>");
-}, 1000);
+        var seconds = fixIntegers(difference % 60);
+        difference = Math.floor(difference / 60);
 
-function fixIntegers(integer)
-{
-    if (integer < 0)
-        integer = 0;
-    if (integer < 10)
-        return "0" + integer;
-    	return "" + integer;
-}
+        var minutes = fixIntegers(difference % 60);
+        difference = Math.floor(difference / 60);
+
+        var hours = fixIntegers(difference % 24);
+        difference = Math.floor(difference / 24);
+
+        var days = fixIntegers(difference);
+
+        //"Also when the day reaches 00 and their is < 24 hrs to, i would like to add a CSS class to the page somewhere."
+        if (days < 1) {
+            $('.days').addClass("opacity_05");
+        }
+
+        $(".seconds").html(seconds + "<span>SECONDS<\/span>");
+        $(".minutes").html(minutes + "<span>MINUTES<\/span>");
+        $(".hours").html(hours + "<span>HOURS<\/span>");
+        $(".days").html(days + "<span>DAYS<\/span>");
+    };
+
+    _countdown(); //Execute immediately to avoid 1 sec delay
+    setInterval(_countdown, 1000);
+
+    function fixIntegers(integer) {
+        if (integer < 0)
+            integer = 0;
+        if (integer < 10)
+            return "0" + integer;
+        return "" + integer;
+    }
+
+});
