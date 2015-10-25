57471e1ceb05962b3f71472fa65e3f66dcd50dcd
andynartey <andy.nartey@googlemail.com>
2015-10-25 19:19:11 +0000

    Smooth scroll to position and parallax effect on header

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 $(document).foundation();
 
-// SVG / PNG
-// if(!Modernizr.svg) {
-//     $('img[src*="svg"]').attr('src', function () {
-//     return $(this).attr('src').replace('.svg', '.png');
-//   });
-// }
+$(document).ready(function(){
+
+    // Scroll to any link
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+    $('.hero').parallax({imageSrc: '/images/agent-lineup-v1-small.jpg'});
+});
