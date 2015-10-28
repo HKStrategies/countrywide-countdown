b5c25c8e583854f2b3e235c3143d818f0b8ee8bb
andynartey <andy.nartey@googlemail.com>
2015-10-28 11:52:33 +0000

    Adding vimeo video and character count for textarea

--- js/app.js
+++ js/app.js
@@ -31,4 +31,17 @@ $(document).ready(function(){
 	        $('#Region').append('<option>' + sElement + '</option>');
 	    });
 
+	// Character Count
+
+	var text_max = 300;
+
+	$('#textarea_feedback').html(text_max + ' characters remaining');
+
+    $('#Reason').keyup(function() {
+        var text_length = $('#Reason').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#textarea_feedback').html(text_remaining + ' characters remaining');
+    });
+
 });
